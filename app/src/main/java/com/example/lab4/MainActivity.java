6db4638be09cc22290c913ebbb3f10d62b9a10d5
N-Be <nathanthebellboy@gmail.com>
2020-04-13 16:41:05 -0400

    webview lab

---
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        webView.loadUrl("youtube.com");
+
+    }
+}
